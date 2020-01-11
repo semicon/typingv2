d0cffc33e513cdb3b2b968eaaae28ebf9f561611
semicon <1210@sts.ac.th>
2020-01-11 12:23:35 +0700

    typingv2

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1578719773,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/boxchar-sheet0.png",
+		"images/boxspawn-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/hp-sheet0.png",
+		"images/hp-sheet1.png",
+		"images/enemy-sheet0.png",
+		"images/repay-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/spritefont.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
